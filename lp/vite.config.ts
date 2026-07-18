8f040c991b518d7bc2fcd055f5dd11d8be76bd3d
T4ko0522 <tako.renraku1@gmail.com>
2026-07-19 01:18:35 +0900

    🐛 vp: lazyPluginを追加してformatが正常に働くように修正

--- lp/vite.config.ts
+++ lp/vite.config.ts
@@ -1,7 +1,8 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import { lazyPlugins } from "vite-plus";
 
 export default defineConfig({
-  plugins: [tailwindcss(), sveltekit()],
+  plugins: lazyPlugins(() => [tailwindcss(), sveltekit()]),
 });
